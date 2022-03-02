c440c8b8bb7275fb6247f0cbc13fabeff80d8b8f
Wisp X <wisp-x@qq.com>
2022-03-02 16:12:36 +0800

    :tada: Initial commit.

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    lang: 'zh-CN',
+    title: 'Lsky Pro',
+    description: '兰空图床使用文档',
+
+    theme: '@vuepress/theme-default',
+    themeConfig: {
+        logo: 'https://avatars.githubusercontent.com/u/100565733?s=200',
+    },
+}
