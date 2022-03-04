371cc21b14117cf4e1abe88146749f64ca8e9c66
Akatsukiro <aka@bep.ink>
2022-03-04 21:44:30 +0800

    fix

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -63,7 +63,7 @@ export default defineUserConfig<DefaultThemeOptions>({
                     text: '快速入门',
                     link: '/docs/v2/',
                     children: [
-                        '/docs/v2/installation.md','/docs/v2/2222.md'
+                        '/docs/v2/installation.md'
                     ]
                 },
             ],
