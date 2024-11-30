ef78d5a0bc94a1d330f396a92b06ce9edcafd123
Wisp X <wisp-x@qq.com>
2024-11-30 15:57:33 +0800

    wip

--- src/.vitepress/theme/Layout.vue
+++ src/.vitepress/theme/Layout.vue
@@ -60,6 +60,26 @@ provide('toggle-appearance', async ({ clientX: x, clientY: y }: MouseEvent) => {
   color: white;
 }
 
+:root {
+  --vp-home-hero-name-color: transparent;
+  --vp-home-hero-name-background: -webkit-linear-gradient(120deg, #bd34fe 30%, #41d1ff);
+
+  --vp-home-hero-image-background-image: linear-gradient(-45deg, #bd34fe 50%, #47caff 50%);
+  --vp-home-hero-image-filter: blur(110px);
+}
+
+@media (min-width: 640px) {
+  :root {
+    --vp-home-hero-image-filter: blur(130px);
+  }
+}
+
+@media (min-width: 960px) {
+  :root {
+    --vp-home-hero-image-filter: blur(150px);
+  }
+}
+
 ::view-transition-old(root),
 ::view-transition-new(root) {
   animation: none;
--- src/zh/index.md
+++ src/zh/index.md
@@ -40,25 +40,3 @@ features:
     icon: 🔍
     details: 为了防止他人上传违规图片，程序集成了阿里云、腾讯云、Nsfw.js 图片审核功能，可以选择发现违规图片后删除或直接拒绝上传。
 ---
-
-<style>
-:root {
-  --vp-home-hero-name-color: transparent;
-  --vp-home-hero-name-background: -webkit-linear-gradient(120deg, #bd34fe 30%, #41d1ff);
-
-  --vp-home-hero-image-background-image: linear-gradient(-45deg, #bd34fe 50%, #47caff 50%);
-  --vp-home-hero-image-filter: blur(110px);
-}
-
-@media (min-width: 640px) {
-  :root {
-    --vp-home-hero-image-filter: blur(130px);
-  }
-}
-
-@media (min-width: 960px) {
-  :root {
-    --vp-home-hero-image-filter: blur(150px);
-  }
-}
-</style>
