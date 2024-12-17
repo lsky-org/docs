0cffa0c89a6bc4344a3272cdab514a870d74ede7
Wisp X <wisp-x@qq.com>
2024-12-17 13:22:22 +0800

    wip

--- src/.vitepress/zh.ts
+++ src/.vitepress/zh.ts
@@ -20,7 +20,7 @@ export const zh = defineConfig({
     },
 
     footer: {
-      copyright: `版权所有 © 2018-${new Date().getFullYear()} <a href="https://huohuastudio.com">火花网络科技</a>`
+      copyright: `版权所有 © 2018-${new Date().getFullYear()} <a href="https://huohuastudio.com">Spark Studio</a>`
     },
 
     docFooter: {
